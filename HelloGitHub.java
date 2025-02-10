4876255afb427c5095081ddddfa1106a6d9610a4
Marvin M. Agudines <140509436+StephMarvin@users.noreply.github.com>
2025-02-10 23:37:41 +0800

    Added a new feature: Prints the current date and time by Marvin Agudines

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,5 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HelloGitHub {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        // By Marvin Agudines
+
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Format the date and time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        
+        System.out.println("Hello GitHub!");
+
+        // Prints currrent date and time
+        System.out.println("Current Date and Time: " + formattedDateTime);
+
+        
     }
 }
